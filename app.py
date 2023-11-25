bf5fab9c15c430fd5083e51f87713f1453bc4d3b
Arely-salazar <79338066+Arely-salazar@users.noreply.github.com>
2023-11-25 06:05:32 +0000

    base inicial

--- app.py
+++ app.py
@@ -9,3 +9,17 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return app.send_static_file("index.html")
+
+menu_op = int(input("-----Bienvenido al juego------\n1.rock \n2.paper \n2.scissors:"))
+
+if menu_op == 1:
+    print("")
+
+elif menu_op == 2:
+    print("")
+    exit()
+elif menu_op == 3:
+    print("")
+else:
+    print("Opcion invalida")
+    exit()
