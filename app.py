2a8b2eef1843297d023d2ab9429fe18dd88750d3
Arely-salazar <79338066+Arely-salazar@users.noreply.github.com>
2023-11-26 02:25:10 +0000

    finaliazado

--- app.py
+++ app.py
@@ -10,17 +10,81 @@ app = Flask(__name__)
 def hello():
     return app.send_static_file("index.html")
 
-print("-----Bienvenido al juego------. \nDe las siguientes opciones: \nRock \nPaper \nScissors")
-menu_op = input("Elija una: ")
+import random
 
-if menu_op.lower() == "rock":
-    print("rock")
 
-elif menu_op.lower() == "paper":
-    print("paper")
+#definimos una funcion para el elemento random
+def random_element():
+    
+    return random.choice(["rock", "paper", "scissors"]) #elementos random para el juego
 
-elif menu_op.lower() == "scissors":
-    print("scissors")
+#definimos una funcion para jugar
+def jugar():
+    
+    print("-----Bienvenido al juego------. \nDe las siguientes opciones: \nRock \nPaper \nScissors")
+    menu_op = input("Elija una: ")
 
-else:
-    print("Opción incorrecta")
+    elemento = random_element() #variable para el elemento random
+    global victorias #variable global para contar las victorias
+
+    if menu_op.lower()== "rock":
+        if elemento == "rock": #si el elemento random es igual a rock es empate
+            print(f"Opción del oponente: {elemento}")
+            print("Empate")
+
+        elif elemento == "paper": #si el elemento random es igual a paper pierde
+            print(f"Opción del oponente: {elemento}")
+            print("Perdiste")
+            
+        elif elemento == "scissors": #si el elemento random es igual a scissors gana
+            print(f"Opción del oponente: {elemento}")
+            print("Ganaste")
+            victorias += 1 #se suma una victoria a la variable global
+
+    elif menu_op.lower() == "paper":
+        if elemento == "rock":
+            print(f"Opción del oponente: {elemento}")
+            print("Ganaste")
+            victorias += 1
+            
+        elif elemento == "paper":
+            print(f"Opción del oponente: {elemento}")
+            print("Empate")
+            
+        elif elemento == "scissors":
+            print(f"Opción del oponente: {elemento}")
+            print("Perdiste")
+            
+
+    elif menu_op.lower() == "scissors":
+        if elemento == "rock":
+            print(f"Opción del oponente: {elemento}")
+            print("Perdiste")
+
+        elif elemento == "paper":
+            print(f"Opción del oponente: {elemento}")
+            print("Ganaste")
+            victorias += 1
+
+        elif elemento == "scissors":
+            print(f"Opción del oponente: {elemento}")
+            print("Empate")
+
+    else:
+        print("Opción incorrecta")
+
+
+victorias = 0 #variable para contar las victorias
+rondas = 0 #variable para contar las rondas
+
+#bucle para jugar de nuevo
+while True:
+    jugar()
+
+    rondas += 1 #se suma una ronda
+    
+    jugar_de_nuevo = input("¿Quieres jugar de nuevo? (SI/NO): ")
+    if jugar_de_nuevo.lower() != 'si':
+        break       #si la respuesta es diferente a si se rompe el bucle
+
+print(f"Puntuacion final: {victorias}. \nRondas jugadas: {rondas}")
